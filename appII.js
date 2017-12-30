6ee09b2ea291a29dc092cfef311aec7a605c2b42
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-29 20:27:38 -0800

    :(codeNotrunning

--- appII.js
+++ appII.js
@@ -8,18 +8,21 @@ var allImages = ['bag.jpg','bananas.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfas
 //create an empty array for the objects with data stored in them
 var allPictureObjects = [];
 
+var counter = 0;
+
 var list = [];
 if (localStorage.list) {
   list = JSON.purse(localStorage.list);
 } else {
   list = [];
+
 }
 console.log(list);
 
 //create a CONSTRUCTOR funtion
 function imagesConstructor(filepath){
   this.name = filepath.split('.')[0];
-  this.filepath = 'img/' + filepath;
+  this.filepath = 'img' + filepath;
   this.numClicks = 0;
   this.numShown = 0;
   allPicture.push(this);
@@ -28,8 +31,8 @@ function imagesConstructor(filepath){
 
 //create a function for puducts arrays
 var createPicturesArray = function() {
-  for(var i = 0; i < allStuff.length; i++){
-    new imagesConstructor(allStuff[i]);
+  for(var i = 0; i < allImages.length; i++){
+    new imagesConstructor(allImages[i]);
   }
   console.log('allPictureObjects :: ', allPictureObjects);
 };
@@ -49,4 +52,264 @@ var randomOne, randomTwo, randomThree;
 var showImages = function(){
   //get random #s from 0-to-20 for firstLocation
   randomOne = Math.floor(Math.random() * allImages.length);
+  //display firstLocation images
+  firstLocation.src = allPictureObjects[firstLocation].filepath;
+  // + firstLocation to numShown globalcounter in allPictureObjects array
+  allPictureObjects[randomOne].numShown += 1;
+
+
+//reapeate above step but add logic for secondLocation not= firstLocation thirdLocation
+  randomTwo = Math.floor(Math.random() * allImages.length);
+  while (randomOne === randomTwo) {
+    randomTwo =  Math.floor(Math.random() * allImages.length);
+  };
+
+  secondLocation.src = allPictureObjects[randomTwo].filepath;
+  allPictureObjects[randomTwo].numShown += 1;
+
+  //reapeate above step but add logic for secondLocation not= firstLocation and secondLocation
+  randomThree = Math.floor(Math.random() * allImages.length);
+  //display secondLocation images & setup to not= firstLocation and thirdLocation
+  while (randomOne === randomTwo || randomTwo === randomThree) {
+    randomThree = Math.floor(Math.random() * allImages.length);
+}
+
+thirdLocation.src = allPictureObjects[randomThree].filepath;
+allPictureObjects[randomThree].numShown += 1;
+
+console.log([randomOne, randomTwo, randomThree]);
+};
+
+//call showImages function
+showImages();
+
+
+//var counter = 0;--- moved to top/ good location for global variables
+
+
+//create event listeners and set agains function
+firstLocation.addEventListener('click', function(){
+  allPictureObjects[randomOne].numClicks += 1;
+  counter += 1;
+  if (counter >= 25) {
+    //remove the imagesChanger
+    save();
+    document.getElementById('images').remove();
+    //collect data
+    collectData();
+    console.log('numberOfTimesShown :: ', numberOfTimesShown);
+    console.log('numberOfTimesClicked :: ', numberOfTimesClicked);
+    // Show chart
+    showMyChart();
+    showMyCharttwo();
+  }
+  showImages();
+});
+
+secondLocation.addEventListener('click', function(){
+  allPictureObjects[randomTwo].numClicks += 1;
+  counter += 1;
+  if (counter >= 25) {
+    //remove the images
+    save();
+    document.getElementById('images').remove();
+    //collect data
+    collectData();
+    console.log('numberOfTimesShown :: ', numberOfTimesShown);
+    console.log('numberOfTimesClicked :: ', numberOfTimesClicked);
+    // Show chart
+    showMyChart();
+    showMyCharttwo();
+  }
+  showImages();
+});
+
+thirdLocation.addEventListener('click', function(){
+  allPictureObjects[randomThree].numClicks += 1;
+  counter += 1;
+  if (counter >= 25) {
+    //remove the images
+    save();
+    document.getElementById('images').remove();
+    //collect data
+    collectData();
+    console.log('numberOfTimesShown :: ', numberOfTimesShown);
+    console.log('numberOfTimesClicked :: ', numberOfTimesClicked);
+    // Show chart
+    showMyChart();
+    showMyCharttwo();
+  }
+  showImages();
+});
+
+var numberOfTimesShown = [];
+var numberOfTimesClicked = [];
+
+function numberOfTimesClickedOut(){
+  return numberOfTimesClicked;
+};
+numberOfTimesClickedOut();
+
+//*********************************************************************************//
+var collectData = function() {
+  for (var i = 0; i < allPictureObjects.length; i++) {
+    numberOfTimesShown.push(allPictureObjects[i].numShown);
+    numberOfTimesClicked.push(allPictureObjects[i].numClicks);
+  };
+};
+
+//var dataStringified = JSON.stringify(numberOfTimesClicked);
+// var myBarChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: data,
+//     options: options
+// });
+
+//var canvas = document.getElementById('chart1');
+//var ctx = canvas.getContext('2d');
+function showMyChart (){
+  console.log('INSIDE CHART', numberOfTimesClicked);
+  new Chart(ctx, {
+
+    type: 'bar',
+    data: {
+      labels:['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'],
+      datasets: [{
+        label: ['Times Clicked'],
+        data: numberOfTimesClicked,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    maintainAspectRatio: true,
+    options: {
+      responsive: true,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+};
+
+function showMyCharttwo (){
+  console.log('INSIDE CHART', numberOfTimesShown);
+  new Chart(ctx, {
+
+    type: 'bar',
+    data: {
+      labels:['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'],
+      datasets: [{
+        label: ['Times Shown'],
+        data: numberOfTimesShown,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    maintainAspectRatio: false,
+    options: {
+      responsive: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+};
+
+function save(){
+  localStorage.list = JSON.stringify(allPictureObjects);
 }
